6400641ad9468243f79ce4b1deabb64687c58292
Aaron Johnson <adjgamer321@gmail.com>
2023-05-31 18:01:11 -0400

    Initial Setup of Bot

---
+++ index.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+const {REST} = require('@discordjs/rest');
+const {Routes} = require("discord-api-types/v10");
+const { Client, Intents, Collection } = require("discordjs");
+const {Player } = require("discord-player");
+
+
